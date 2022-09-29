6d1a9f3f18306cea4676d7d6a35dbf0d2c32e530
Donovan <donovangriffin04@gmail.com>
2022-09-29 16:30:05 -0700

    refactored the input values so it is not hard coded

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const btn = document.querySelector('button');
-const cad = document.querySelector('.cad-price')
+const cad = document.querySelector('.cad-price');
 const gallons = document.querySelector('.gallons');
-const us = document.querySelector('.us-price')
+const us = document.querySelector('.us-price');
+const formData = {};
 
-
-const formData = {}
-    gallons.addEventListener('input', e => {
-    formData['gallons'] = parseFloat(e.target.value)
+for (let input of [ gallons, cad, us ]) {
+    input.addEventListener('input', (e) => {
+    formData[e.target.name] = e.target.value
 })
-
-cad.addEventListener('input', e => {
-    formData['Cad price'] = parseFloat(e.target.value)
-})
-
-us.addEventListener('input', e => {
-    formData['Us price'] = parseFloat(e.target.value)
-})
+ }
